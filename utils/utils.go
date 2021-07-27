80328773d55556d1fef8f3b1a80660fd610b87a1
Eloy Tolosa <eloytolosawork@gmail.com>
2021-07-27 16:02:40 +0200

    New: util function to get logical position
    New function added that transforms an absolute position in the screen to
    a logical position in the table.

--- utils/utils.go
+++ utils/utils.go
@@ -1,22 +1,26 @@
 package utils
 
 import (
-    "log"
-    
+	"ChessEngine/globals"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
-    "github.com/hajimehoshi/ebiten/v2"
 )
 
-// THis call fatals when error
+// Returns a ebiten.Image object from a file path.
+// Fatals if error
 func NewImage(path string) (img *ebiten.Image) {
-
-    img, _, err := ebitenutil.NewImageFromFile(path)
-    if err != nil {
-        log.Fatalln(err.Error())
-    }
-    // log.Printf("Loading image %s\n", path)
-    return img
-
+	img, _, err := ebitenutil.NewImageFromFile(path)
+	if err != nil {
+		log.Fatalln(err.Error())
+	}
+	return img
 }
 
-
+// Returns the logical position (x,y) from a pixel coordinate
+func GetLogicalPosition(absX, absY int) (logX, logY int) {
+	cWidth := int(globals.WindowWidth / globals.TableDim)
+	cHeight := int(globals.WindowHeight / globals.TableDim)
+	return (absX / cWidth), (absY / cHeight)
+}
